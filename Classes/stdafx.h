8a017b504ea7a1275ad510d30ffcd7e961698415
Lee Kwenwon <highfence@naver.com>
2016-12-19 14:12:36 +0900

    initial commit

---
+++ Classes/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable : 4100 4245 4819)
+#include "cocos2d.h"
+#pragma warning(pop)
+USING_NS_CC;
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+#include <sql.h>
+#include <sqlext.h>
